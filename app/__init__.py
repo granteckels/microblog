73580b9b5cf8cffdffdd240612365ff6f33446a8
Grant Eckels <geckels1@gmail.com>
2023-12-20 21:09:35 -0600

    Chapter 10 Skipped thread for sending email

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -13,5 +14,7 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+
+mail = Mail(app)
 
 from app import routes, models, errors
