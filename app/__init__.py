0207fe0daabb776b657b1b1ff8ffc5a63bd3ea1f
Grant Eckels <geckels1@gmail.com>
2023-12-22 19:13:22 -0600

    Chapter 13 Skipped adding new commands

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 
@@ -15,9 +19,12 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l("Please log in to access this page.")
 
 mail = Mail(app)
 
 moment = Moment(app)
+
+babel = Babel(app, locale_selector=get_locale)
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 3
+    LANGUAGES = ['en', 'fr']
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
